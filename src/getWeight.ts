6fd65894de1b520e5aace0e8ddf7e29722585237
Miguel <miguespinoza1@gmail.com>
2019-10-27 20:40:04 -0600

    metadata

--- src/getWeight.ts
+++ src/getWeight.ts
@@ -1,7 +1,19 @@
-export default function getWeight(d: any) {
+type Rules = {
+  [word: string]: number;
+};
+
+export default function getWeight(d: any, rules: Rules) {
+  const score = Object.entries(rules).reduce<number>((out, v) => {
+    if (new RegExp(v[0]).test(d.delito)) {
+      return out + v[1];
+    }
+    return out;
+  }, 0);
+  if (score != null) {
+    return score;
+  }
   if (d.categoria_delito === "DELITO DE BAJO IMPACTO") {
     return 1;
-  } else {
-    return 10;
   }
+  return 2;
 }
